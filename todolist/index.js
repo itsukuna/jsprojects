be4d0ec3a1d1a774f227354212ddab3f1b03cb74
itsukuna <ayush.arya.16@gmail.com>
2024-09-16 18:12:49 +0530

    todo list

---
+++ todolist/index.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.root');
+const heading = document.createElement('h1');
+
+heading.textContent="To-do List";
+
+const inputArea = document.createElement('div');
+const inputField = document.createElement('input');
+const addButton = document.createElement('button');
+const taskList = document.createElement('ul');
+
+addButton.textContent= "Add to list"
+addButton.addEventListener('click', function(){
+    const task = inputField.value
+
+    if (task){
+        const taskItem = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent= 'delete';
+        
+        deleteButton.addEventListener('click', function(){
+            taskList.removeChild(taskItem)
+        });
+        
+        taskItem.textContent =task;
+        taskItem.appendChild(deleteButton);
+        taskList.appendChild(taskItem);
+
+        inputField.value = ''
+    } else {
+        alert('please enter task')
+    }
+})
+
+inputArea.append(inputField, addButton)
+container.append(heading, inputArea, taskList)
+
